dfae3a7ed5b038c893298c8f0096c6af436f2a0e
AJaySRao <98860192+AJaySRao@users.noreply.github.com>
2022-02-22 09:34:05 +0530

    Extraction
    Extracted colors from a image using colorgram module

--- color.py
+++ color.py
@@ -1,6 +1,6 @@
 import colorgram
 
-# Extract 256 colors from an image.
+# Extract 25 colors from an image.
 # colors = colorgram.extract('spotsExtract.jpg', 25)
 # list_colors = []
 # for color in colors:
@@ -9,8 +9,7 @@ import colorgram
 #     b = color.rgb.b
 #     new_color = (r, g, b)
 #     list_colors.append(new_color)
-#
-# print(list_colors)
+
 
 rgb_colors = [(236, 35, 108), (221, 232, 237), (145, 28, 64), (239, 75, 35), (6, 148, 93), (232, 238, 234), (231, 168, 40), (184, 158, 46), (44, 191, 233), (27, 127, 195), (126, 193, 74), (253, 223, 0), (85, 28, 93), (173, 36, 97), (246, 219, 44), (44, 172, 112), (215, 130, 165), (215, 56, 27), (235, 164, 191), (156, 24, 23), (21, 188, 230), (238, 169, 157), (162, 210, 182)]
 
